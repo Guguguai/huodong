292f4cc1394f4a4ca0ad71319d3669f96cdd5ac0
guguguai <2907208640@qq.com>
2018-12-28 10:48:58 +0800

    mock

---
+++ mock/mockdata.js
@@ -0,0 +1,16 @@
+const mockdata_1 = {
+  result: 1,
+  msg: 'OK',
+  list: {
+    name: 'didi'
+  }
+}
+const mockdata_2 = {
+  result: 2,
+  msg: 'ERROR',
+  list: {}
+}
+module.exports = {
+  mockdata_2: mockdata_2,
+  mockdata_1: mockdata_1
+}
